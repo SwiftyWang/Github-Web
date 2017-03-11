b8f14911131a9fd8b1b61e3661918275076ca5f7
Swifty Wang <wsk900906@gmail.com>
2017-03-11 23:56:10 +0800

    update.

--- app/src/main/java/com/swifty/mygithub/MainActivity.java
+++ app/src/main/java/com/swifty/mygithub/MainActivity.java
@@ -2,12 +2,15 @@ package com.swifty.mygithub;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.widget.Toast;
 
 import com.swifty.mygithub.storage.Pref;
 
 public class MainActivity extends AppCompatActivity {
+    boolean doubleBackToExitPressedOnce = false;
 
     public void addSplashFragment() {
         Fragment fragment = new SplashFragment();
@@ -32,13 +35,25 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        super.onBackPressed();
         Fragment fragment = getFragmentManager().findFragmentById(R.id.container);
         if (fragment instanceof IBackableFragment) {
             if (((IBackableFragment) fragment).onBackPressed()) {
                 return;
             }
         }
-        super.onBackPressed();
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, getString(R.string.press_again_exit), Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
     }
 }
