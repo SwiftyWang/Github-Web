dce8b822355b5ec5989dd7a9fde70943f3d74cdb
SwiftyWang <swang@seekerscapital.com>
2019-09-12 10:03:51 +0800

    change to new admob id

--- app/src/main/java/com/swifty/mygithub2/APIClass.java
+++ app/src/main/java/com/swifty/mygithub2/APIClass.java
@@ -4,6 +4,6 @@ package com.swifty.mygithub2;
  * Created by swifty on 16/12/2016.
  */
 public class APIClass {
-    public static final String PUBLISHER_ID = "ca-app-pub-7379141907336355~1948493423";
+    public static final String PUBLISHER_ID = "ca-app-pub-7379141907336355~8997529210";
     public static final String MEIZU_DEVICE_ID = "1D8B538A6D6EB466CBB5C7759EFC76A5";
 }
